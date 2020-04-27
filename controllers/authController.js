641a06b3147b4f5c81fff83693f72fa81a8cc19f
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-04-27 10:51:12 +0600

    signup validation done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,26 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const { validationResult } = require('express-validator')
-const errorFormatter = require('../utils/validationErrorFormatter')
+const { validationResult } = require("express-validator");
+const errorFormatter = require("../utils/validationErrorFormatter");
 
 exports.signupGetController = (req, res, next) => {
-  res.render("pages/auth/signup", { title: "Create a new Account", error: {}});
+  res.render("pages/auth/signup", { title: "Create a new Account", error: {} });
 };
 
 exports.signupPostController = async (req, res, next) => {
-  let errors = validationResult(req).formatWith(errorFormatter)
-  if(!errors.isEmpty()){
-    return res.render('pages/auth/signup', {title: 'Create a new account', error:errors.mapped()})
-  }
   let { username, email, password } = req.body;
+  let errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return res.render("pages/auth/signup", {
+      title: "Create a new account",
+      error: errors.mapped(),
+      value: {
+        username,
+        email,
+        password,
+      },
+    });
+  }
 
   try {
     // 11 means times of encryption
@@ -26,7 +34,10 @@ exports.signupPostController = async (req, res, next) => {
 
     let createdUser = await user.save(); // return promise and store it to createdUser
     console.log("User created successfully", createdUser);
-    res.render("pages/auth/signup", { title: "Create a new Account", error: {} });
+    res.render("pages/auth/signup", {
+      title: "Create a new Account",
+      error: {},
+    });
   } catch (e) {
     console.log(e);
     next(e);
@@ -34,30 +45,30 @@ exports.signupPostController = async (req, res, next) => {
 };
 
 exports.loginGetController = (req, res, next) => {
-  res.render('pages/auth/login', {title: 'Login'})
+  res.render("pages/auth/login", { title: "Login" });
 };
 exports.loginPostController = async (req, res, next) => {
-  let { email, password } = req.body
+  let { email, password } = req.body;
 
-  try{
-    let user = await User.findOne({ email })
-    if(!user){
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
       return res.json({
-        message: 'Invalid Credential'
-      })
+        message: "Invalid Credential",
+      });
     }
 
-    let match = await bcrypt.compare(password, user.password)
-    if(!match){
+    let match = await bcrypt.compare(password, user.password);
+    if (!match) {
       return res.json({
-        message: 'Invalid credential',
-      })
+        message: "Invalid credential",
+      });
     }
-    console.log('Successfully logged in', user)
-    res.render('pages/auth/login', {title: 'Login'})
-  } catch(e){
-    console.log(e)
-    next(e)
+    console.log("Successfully logged in", user);
+    res.render("pages/auth/login", { title: "Login" });
+  } catch (e) {
+    console.log(e);
+    next(e);
   }
 };
 
